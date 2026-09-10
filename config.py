fe8c11ba367b7a58a77fdf29a0693f32ad34c343
🟢 B A B A   J I ⚡ <dabhimanyu40@gmail.com>
2026-09-10 16:26:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,11 @@ load_dotenv()
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 MONGO_URL = os.getenv("MONGO_URL")
+
 LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
+
+SESSION_STRING = os.getenv("SESSION_STRING")
